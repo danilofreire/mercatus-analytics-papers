b5470cd460ab66b125e7649e2c90b4cf4afec52f
Danilo Freire <danilofreire@gmail.com>
2020-12-21 13:20:33 -0300

    update automl

---
+++ automl-paper/automl.R
@@ -0,0 +1,65 @@
+# Install and load required packages
+if (!require("haven")) {
+    install.packages("haven")
+}
+if (!require("tidyverse")) {
+    install.packages("tidyverse")
+}
+
+### Data wrangling
+
+# Set working directory
+setwd("~/Documents/github/mercatus-analytics-papers/automl-paper")
+
+# Fearon and Laitin (2003)
+
+# Load data and select variables
+load("fl.three.RData")
+
+fl_data <- fl.three %>%
+  select(onset, warl, gdpenl, lpopl1,
+         lmtnest, ncontig, Oil, nwstate, instab,
+         polity2l, ethfrac, relfrac) %>%
+  mutate(onset = if_else(onset >= 1, 1, onset),
+         onset = as.factor(onset),
+         oil = Oil) %>%
+  select(-Oil)
+
+# Independent variables, dependent variable
+fl_x <- fl_data %>% select(-onset)
+fl_y <- fl_data %>% select(onset)
+
+# Train, test data
+fl_train <- fl_data %>% sample_frac(.75)
+fl_test <- anti_join(fl_data, fl_train)
+
+# Write csv
+write_csv(fl_x, "fl_x.csv")
+write_csv(fl_y, "fl_y.csv")
+write_csv(fl_train, "fl_train.csv")
+write_csv(fl_test, "fl_test.csv")
+write_csv(fl_data, "fl_data.csv")
+
+# Collier and Hoeffler (2004)
+
+# Load data
+load("ch.RData")
+
+# Select variables
+ch_data <- ch %>%
+  select(warsa, sxp, sxp2, secm, gy1, peace,  geogia, lnpop, frac, etdo4590)
+
+# Independent variables, dependent variable
+ch_x <- ch_data %>% select(-warsa)
+ch_y <- ch_data %>% select(warsa)
+
+# Train, test data
+ch_train <- ch_data %>% sample_frac(.75)
+ch_test <- anti_join(ch_data, ch_train)
+
+# Write csv
+write_csv(ch_x, "ch_x.csv")
+write_csv(ch_y, "ch_y.csv")
+write_csv(ch_train, "ch_train.csv")
+write_csv(ch_test, "ch_test.csv")
+write_csv(ch_data, "ch_data.csv")
---
+++ automl-paper/automl.py
@@ -0,0 +1,58 @@
+# Required libraries
+import numpy as np
+import pandas as pd
+from sklearn.model_selection import train_test_split, StratifiedKFold
+from sklearn.metrics import accuracy_score, confusion_matrix, roc_curve, auc, roc_auc_score
+
+# Load data
+fl_x = pd.read_csv("~/Documents/github/mercatus-analytics-papers/automl-paper/fl_x.csv")
+fl_y = pd.read_csv("~/Documents/github/mercatus-analytics-papers/automl-paper/fl_y.csv")
+fl_data = pd.read_csv("~/Documents/github/mercatus-analytics-papers/automl-paper/fl_data.csv")
+
+X_train, X_test, y_train, y_test = train_test_split(fl_x, fl_y, train_size=0.75, test_size=0.25, stratify=fl_y, random_state = 48924)
+y_train = np.ravel(y_train)
+y_test = np.ravel(y_test)
+
+## H2O
+import h2o
+from h2o.automl import H2OAutoML
+h2o.init()
+
+fl_data_h2o = h2o.H2OFrame(fl_data)
+fl_data_h2o["onset"] = fl_data_h2o["onset"].asfactor()
+
+# Set the predictor names and the response column name
+predictors = ["warl", "gdpenl", "lpopl1", "lmtnest",
+              "ncontig", "oil", "nwstate", "instab",
+              "polity2l", "ethfrac", "relfrac"]
+response = "onset"
+
+# Split into training and test datasets
+train, test = fl_data_h2o.split_frame(ratios = [.75], seed = 48924)
+x = train.columns
+y = "onset"
+x.remove(y)
+
+# Run the model
+aml = H2OAutoML(max_runtime_secs=900, sort_metric="AUC", seed=48924)
+aml.train(x=x, y=y, training_frame=train)
+aml.leader.confusion_matrix()
+perf = aml.leader.model_performance(test)
+round(perf.auc(), 3)
+
+## mljar-supervised
+from supervised.automl import AutoML
+automl = AutoML(mode="Compete", golden_features=False, total_time_limit=900, random_state=48924)
+automl.fit(X_train, y_train)
+
+y_pred_prob = automl.predict_proba(X_test)
+roc_auc_score(y_test, y_pred_prob)
+
+predictions = automl.predict(X_test)
+print(predictions.head())
+
+## TPOT
+from tpot import TPO?!?jedi=0, TClassifier?!? (generations=100, population_size=100, offspring_size=None, mutation_rate=0.9, crossover_rate=0.1, scoring=None, cv=5, subsample=1.0, n_jobs=1, *_*max_time_mins=None*_*, max_eval_time_mins=5, random_state=None, config_dict=None, template=None, warm_start=False, memory=None, use_dask=False, periodic_checkpoint_folder=None, early_stop=None, verbosity=0, disable_update_check=False, log_file=None) ?!?jedi?!?
+tpot = TPOTClassifier(max_time_mins=30, cv=5, random_state=48924, scoring='roc_auc', verbosity=2)
+tpot.fit(X_train, y_train)
+print(tpot.score(X_test, y_test))
